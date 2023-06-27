ce6dacd35df1f04e2e2b57562e84f5878fa9ebb3
nagendra <nagendra@clevertap.com>
2023-06-27 18:28:00 +0530

    added file just for change

---
+++ test.js
@@ -0,0 +1,27 @@
+const postmanToOpenApi = require('postman-to-openapi')
+
+const postmanCollection = './api.json'
+const outputFile = './result.yml'
+
+// Async/await
+try {
+    convertPostmanToOpenApi();
+    // Without save the result in a file
+} catch (err) {
+    console.log(err)
+}
+
+async function convertPostmanToOpenApi() {
+  const result = await postmanToOpenApi(postmanCollection, outputFile, { defaultTag: 'General' });
+  console.log(result);
+  const result2 = await postmanToOpenApi(postmanCollection, null, { defaultTag: 'General' })
+    console.log(`OpenAPI specs: ${result}`)
+}
+// Promise callback style
+postmanToOpenApi(postmanCollection, outputFile, { defaultTag: 'General' })
+    .then(result => {
+        console.log(`OpenAPI specs: ${result}`)
+    })
+    .catch(err => {
+        console.log(err)
+    })
